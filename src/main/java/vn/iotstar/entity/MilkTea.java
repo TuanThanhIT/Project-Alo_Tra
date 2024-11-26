fb14b053c6c5607296fce60bfd74d53c293cbfae
LyHung10 <lyhung10nctlop95@gmail.com>
2024-11-26 18:26:01 +0700

    merge code thanh va fix confict

--- src/main/java/vn/iotstar/entity/MilkTea.java
+++ src/main/java/vn/iotstar/entity/MilkTea.java
@@ -1,11 +1,18 @@
 package vn.iotstar.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
 @Table(name = "MilkTea")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MilkTea {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
