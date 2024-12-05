5ef8537ad7a2826df4b11877707a383d5a360ae6
LyHung10 <lyhung10nctlop95@gmail.com>
2024-12-05 19:05:52 +0700

    sửa WaitingController

--- src/main/java/vn/iotstar/controllers/WaitingController.java
+++ src/main/java/vn/iotstar/controllers/WaitingController.java
@@ -12,29 +12,22 @@ import vn.iotstar.entity.User;
 @Controller
 @RequestMapping("/waiting")
 public class WaitingController {
-    
-    @GetMapping
-    public String handleWaiting(HttpServletRequest request, HttpServletResponse response) {
-        // Lấy session hiện tại
-        HttpSession session = request.getSession(false);
 
-        // Kiểm tra xem session có tồn tại và "account" có phải là một User hay không
-        if (session != null && session.getAttribute("account") instanceof User) {
-            User user = (User) session.getAttribute("account");
+	@GetMapping
+	public String handleWaiting(HttpServletRequest request, HttpServletResponse response) {
+		// Lấy session hiện tại
+		HttpSession session = request.getSession(false);
 
-            // Kiểm tra vai trò của người dùng và chuyển hướng tới trang phù hợp
-            if (user.getRoleID() == 1) {
-                return "redirect:/user/home";  // Nếu là admin
-            } 
-            else if (user.getRoleID() == 2) {
-                return "redirect:/user/home";  // Nếu là người bán
-            }
-            else if (user.getRoleID() == 3) {
-                return "redirect:/user/home";  // Nếu là người dùng
-            }
-        }
+		// Kiểm tra xem session có tồn tại và "account" có phải là một User hay không
+		if (session != null && session.getAttribute("account") instanceof User) {
+			User user = (User) session.getAttribute("account");
 
-        // Nếu không có tài khoản người dùng trong session, chuyển hướng về trang đăng nhập
-        return "redirect:/login";
-    }
+			return "redirect:/user/home"; // Nếu là admin
+
+		}
+
+		// Nếu không có tài khoản người dùng trong session, chuyển hướng về trang đăng
+		// nhập
+		return "redirect:/login";
+	}
 }
